bdf57ce6afa7ad7fdf245a2ec3e5800d68eb898c
shine910816 <shine910816@163.com>
2020-04-14 18:06:17 +0800

    add file

---
+++ api/menu/Course/act/BroadcomCourse_CombineListAction.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * 排课信息列表画面(排课检证用组合条件)
+ * @author Kinsama
+ * @version 2020-04-14
+ */
+class BroadcomCourse_CombineListAction extends ActionBase
+{
+
+    /**
+     * 执行主程序
+     * @param object $controller Controller对象类
+     * @param object $user User对象类
+     * @param object $request Request对象类
+     */
+    public function doMainExecute(Controller $controller, User $user, Request $request)
+    {
+        if ($request->hasParameter("multi")) {
+            $ret = $this->_doMultiExecute($controller, $user, $request);
+            if ($controller->isError($ret)) {
+                $ret->setPos(__FILE__, __LINE__);
+                return $ret;
+            }
+        } else {
+            $ret = $this->_doDefaultExecute($controller, $user, $request);
+            if ($controller->isError($ret)) {
+                $ret->setPos(__FILE__, __LINE__);
+                return $ret;
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * 执行参数检测
+     * @param object $controller Controller对象类
+     * @param object $user User对象类
+     * @param object $request Request对象类
+     */
+    public function doMainValidate(Controller $controller, User $user, Request $request)
+    {
+        // 必要参数检证
+        if (!$request->hasParameter("school_id")) {
+            $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: school_id");
+            $err->setPos(__FILE__, __LINE__);
+            return $err;
+        }
+        if (!$request->hasParameter("start_date")) {
+            $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: start_date");
+            $err->setPos(__FILE__, __LINE__);
+            return $err;
+        }
+        if (!$request->hasParameter("end_date")) {
+            $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: end_date");
+            $err->setPos(__FILE__, __LINE__);
+            return $err;
+        }
+        if ($request->hasParameter("multi")) {
+            if (!$request->hasParameter("course_type")) {
+                $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: course_type");
+                $err->setPos(__FILE__, __LINE__);
+                return $err;
+            }
+            $audition_type = null;
+            $item_id = null;
+            if ($request->getParameter("course_type") == BroadcomCourseEntity::COURSE_TYPE_AUDITION_SQUAD) {
+                if (!$request->hasParameter("audition_type")) {
+                    $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: audition_type");
+                    $err->setPos(__FILE__, __LINE__);
+                    return $err;
+                } else {
+                    $audition_type = $request->getParameter("audition_type");
+                }
+            } else {
+                if (!$request->hasParameter("item_id")) {
+                    $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: item_id");
+                    $err->setPos(__FILE__, __LINE__);
+                    return $err;
+                } else {
+                    $item_id = $request->getParameter("item_id");
+                }
+            }
+            $request->setAttribute("course_type", $request->getParameter("course_type"));
+            $request->setAttribute("audition_type", $audition_type);
+            $request->setAttribute("item_id", $item_id);
+        } else {
+            if (!$request->hasParameter("student_id")) {
+                $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: student_id");
+                $err->setPos(__FILE__, __LINE__);
+                return $err;
+            }
+            if (!$request->hasParameter("teacher_member_id")) {
+                $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: teacher_member_id");
+                $err->setPos(__FILE__, __LINE__);
+                return $err;
+            }
+            $request->setAttribute("student_id", $request->hasParameter("student_id"));
+            $request->setAttribute("teacher_member_id", $request->hasParameter("teacher_member_id"));
+        }
+        $school_id = $request->getParameter("school_id");
+        $start_date = $request->getParameter("start_date");
+        $end_date = $request->getParameter("end_date");
+        $post_data = array(
+            "school_id" => $school_id,
+            "start_date" => $start_date,
+            "end_date" => $end_date,
+            "confirm_flg" => "0"
+        );
+        $repond_course_list = Utility::getJsonResponse("?t=D4F1FA27-76D2-3029-4FB9-2FD91B0057B8&m=" . $request->member()->targetObjectId(), $post_data);
+        if ($controller->isError($repond_course_list)) {
+            $repond_course_list->setPos(__FILE__, __LINE__);
+            return $repond_course_list;
+        }
+        $request->setAttribute("course_list", $repond_course_list["course_list"]);
+        return VIEW_DONE;
+    }
+
+    private function _doDefaultExecute(Controller $controller, User $user, Request $request)
+    {
+Utility::testVariable($repond_course_list);
+        $course_list = $request->getAttribute("course_list");
+        $student_id = $request->getAttribute("student_id");
+        $teacher_member_id = $request->getAttribute("teacher_member_id");
+    }
+
+    private function _doMultiExecute(Controller $controller, User $user, Request $request)
+    {
+        $course_list = $request->getAttribute("course_list");
+        $course_type = $request->getAttribute("course_type");
+        $audition_type = $request->getAttribute("audition_type");
+        $item_id = $request->getAttribute("item_id");
+    }
+}
+?>
