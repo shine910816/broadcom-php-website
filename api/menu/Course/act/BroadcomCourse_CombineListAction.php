cb68f3090768b926885054126fef3051aca86dba
Kinsama <shine910816@163.com>
2020-04-15 00:59:12 +0800

    complete single type logic

--- api/menu/Course/act/BroadcomCourse_CombineListAction.php
+++ api/menu/Course/act/BroadcomCourse_CombineListAction.php
@@ -56,15 +56,21 @@ class BroadcomCourse_CombineListAction extends ActionBase
             $err->setPos(__FILE__, __LINE__);
             return $err;
         }
+        if (!$request->hasParameter("student_id")) {
+            $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: student_id");
+            $err->setPos(__FILE__, __LINE__);
+            return $err;
+        }
         if ($request->hasParameter("multi")) {
             if (!$request->hasParameter("course_type")) {
                 $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: course_type");
                 $err->setPos(__FILE__, __LINE__);
                 return $err;
             }
+            $course_type = $request->getParameter("course_type");
             $audition_type = null;
             $item_id = null;
-            if ($request->getParameter("course_type") == BroadcomCourseEntity::COURSE_TYPE_AUDITION_SQUAD) {
+            if ($course_type == BroadcomCourseEntity::COURSE_TYPE_AUDITION_SQUAD) {
                 if (!$request->hasParameter("audition_type")) {
                     $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: audition_type");
                     $err->setPos(__FILE__, __LINE__);
@@ -81,22 +87,16 @@ class BroadcomCourse_CombineListAction extends ActionBase
                     $item_id = $request->getParameter("item_id");
                 }
             }
-            $request->setAttribute("course_type", $request->getParameter("course_type"));
+            $request->setAttribute("course_type", $course_type);
             $request->setAttribute("audition_type", $audition_type);
             $request->setAttribute("item_id", $item_id);
         } else {
-            if (!$request->hasParameter("student_id")) {
-                $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: student_id");
-                $err->setPos(__FILE__, __LINE__);
-                return $err;
-            }
             if (!$request->hasParameter("teacher_member_id")) {
                 $err = $controller->raiseError(ERROR_CODE_USER_FALSIFY, "Parameter missed: teacher_member_id");
                 $err->setPos(__FILE__, __LINE__);
                 return $err;
             }
-            $request->setAttribute("student_id", $request->hasParameter("student_id"));
-            $request->setAttribute("teacher_member_id", $request->hasParameter("teacher_member_id"));
+            $request->setAttribute("teacher_member_id", $request->getParameter("teacher_member_id"));
         }
         $school_id = $request->getParameter("school_id");
         $start_date = $request->getParameter("start_date");
@@ -112,24 +112,50 @@ class BroadcomCourse_CombineListAction extends ActionBase
             $repond_course_list->setPos(__FILE__, __LINE__);
             return $repond_course_list;
         }
+        $request->setAttribute("student_id", $request->getParameter("student_id"));
         $request->setAttribute("course_list", $repond_course_list["course_list"]);
         return VIEW_DONE;
     }
 
     private function _doDefaultExecute(Controller $controller, User $user, Request $request)
     {
-Utility::testVariable($repond_course_list);
         $course_list = $request->getAttribute("course_list");
         $student_id = $request->getAttribute("student_id");
         $teacher_member_id = $request->getAttribute("teacher_member_id");
+        $course_result = array();
+        if (!empty($course_list)) {
+            foreach ($course_list as $course_id => $course_info) {
+                if ($course_info["student_id"] == $student_id || $course_info["teacher_member_id"] == $teacher_member_id) {
+                    $course_item = array();
+                    $course_item["student_id"] = $course_info["student_id"];
+                    $course_item["teacher_member_id"] = $course_info["teacher_member_id"];
+                    $course_item["start_ts"] = strtotime($course_info["course_start_date"]);
+                    $course_item["end_ts"] = strtotime($course_info["course_expire_date"]);
+                    $course_item["date"] = date("Ymd", $course_item["start_ts"]);
+                    $course_result[$course_id] = $course_item;
+                }
+            }
+        }
+        return array(
+            "course_list" => $course_result
+        );
     }
 
     private function _doMultiExecute(Controller $controller, User $user, Request $request)
     {
         $course_list = $request->getAttribute("course_list");
+        $student_id = $request->getAttribute("student_id");
         $course_type = $request->getAttribute("course_type");
         $audition_type = $request->getAttribute("audition_type");
         $item_id = $request->getAttribute("item_id");
+        $course_result = array();
+        if (!empty($course_list)) {
+            foreach ($course_list as $course_id => $course_info) {
+                if ($course_info["student_id"] != $student_id) {
+                    // TODO
+                }
+            }
+        }
     }
 }
 ?>
