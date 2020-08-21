d4b7fbc819113ccb6123452294bd3f4ac8a2965c
a <a>
2020-08-21 15:54:44 +0800

    add history info DBI and entity

---
+++ library/entity/BroadcomHistoryEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 数据库应用类-*
+ * @author Kinsama
+ * @version 2020-08-21
+ */
+class BroadcomHistoryEntity
+{
+    const HISTORY_TYPE_MEMBER = "1";
+    const HISTORY_TYPE_STUDENT = "2";
+    const HISTORY_TYPE_ORDER = "3";
+    const HISTORY_TYPE_ORDER_ITEM = "4";
+    const HISTORY_TYPE_COURSE = "5";
+}
+?>
